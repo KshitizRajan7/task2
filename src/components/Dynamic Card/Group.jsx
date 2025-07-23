b55b1c4f99b4e7f1f75f45c2a9c34de183ea21e2
kshitizrajan7 <kshitizrajandev@gmail.com>
2025-07-23 07:28:43 +0545

    feat: add and arrange group asset from Figma as per design

--- src/components/Dynamic Card/Group.jsx
+++ src/components/Dynamic Card/Group.jsx
@@ -6,11 +6,22 @@ const Group = ({ title }) => {
             {/* clarity */}
             {title == "clarity" && (<div className='relative border'>
                 <div className='w-[257.39px] h-[338.59px] top-[22px] left-[-49px] absolute shadow-lg'>
-                    <img src='clarity/sleeveLine.svg' alt='sleeveLine' className='w-[65.19px] h-[45.69px] top-[182.83px] left-[79.89px] absolute' />
-                    <img src='clarity/sleeve.svg' alt='sleeve' className='w-[64.26px] h-[44.77px] top-[183.33px] left-[80.31px] absolute' />
-                    <img src='clarity/buttonCover2.svg' alt='button2' className='w-[12.04px] h-[12.04px] top-[192.92px] left-[124.66px] absolute' />
-                    <img src='clarity/buttonCover.svg' alt='button1' className='w-[13.05px] h-[13.05px] top-[192.41px] left-[124.15px] absolute' />
-                    <img src='clarity/button.svg' alt='button' className='w-[13.05px] h-[13.05px] top-[190.55px] left-[124.49px] absolute' />
+                    <img src='clarity/body1.svg' alt='body1' className='w-[35.44px] h-[118.12px] top-[179.59px] left-[-28.79px] absolute' />
+                    <img src='clarity/body1Line.svg' alt='body1Line' className='w-[36.39px] h-[119.04px] top-[179.06px] left-[-29.29px] absolute' />
+                    <img src='clarity/body2.svg' alt='body2' className='w-[209.39px] h-[154.38px] top-[160.43px] left-[-22.66px] absolute' />
+                    <img src='clarity/body2Line1.svg' alt='body2Line1' className='w-[33.01px] h-[57.03px] top-[175.88px] left-[134.41px] absolute' />
+                    <img src='clarity/body2Line2.svg' alt='body2Line2' className='w-[210.31px] h-[155.3px] top-[160.02px] left-[-23.02px] absolute' />
+                    <img src='clarity/body2Line3.svg' alt='body2Line3' className='w-[30.68px] h-[68.87px] top-[211.85px] left-[78.47px] absolute' />
+                    <img src='clarity/body3.svg' alt='body3' className='w-[19.01px] h-[4.89px] top-[297.65px] left-[6.65px] absolute' />
+                    <img src='clarity/body3Line.svg' alt='body3Line' className='w-[20.02px] h-[5.86px] top-[297.19px] left-[6.14px] absolute' />
+                    <img src='clarity/color.svg' alt='color' className='w-[35.61px] h-[66.95px] top-[158.04px] left-[41.29px] absolute' />
+                    <img src='clarity/colorLine.svg' alt='colorLine' className='w-[36.59px] h-[67.88px] top-[157.55px] left-[40.84px] absolute' />
+                    <img src='clarity/rightBicepShadow.svg' alt='rightBicepShadow' className='w-[84.65px] h-[128.29px] top-[174.14px] left-[-26.48px] absolute' />
+                    <img src='clarity/rightBicepShadowLine.svg' alt='rightBicepShadowLine' className='w-[85.73px] h-[129.25px] top-[173.69px] left-[-26.95px] absolute' />
+                    <img src='clarity/leftArmShadow.svg' alt='leftArmShadow' className='w-[29.6px] h-[67.94px] top-[212.29px] left-[78.97px] absolute' />
+                    <img src='clarity/leftArmShadowLine.svg' alt='leftArmShadowLine' className='w-[29.6px] h-[67.94px] top-[212.29px] left-[78.97px] absolute' />
+                    <img src='clarity/leftBicepShadow.svg' alt='leftBicepShadow' className='w-[31.96px] h-[58.03px] top-[176.32px] left-[135px] absolute' />
+                    <img src='clarity/leftBicepShadowLine.svg' alt='leftBicepShadowLine' className='w-[33.01px] h-[57.03px] top-[175.88px] left-[134.41px] absolute' />
                     <img src='clarity/armShadow.svg' alt='button' className='w-[40.92px] h-[92.47px] top-[213.5px] left-[146.6px] absolute' />
                     <img src='clarity/armShadow2.svg' alt='button' className='w-[58.95px] h-[73.08px] top-[276.77px] left-[87.08px] absolute' />
                     <img src='clarity/armShadow3.svg' alt='button' className='w-[59.83px] h-[74.03px] top-[276.27px] left-[86.65px] absolute' />
@@ -26,16 +37,39 @@ const Group = ({ title }) => {
                     <img src='clarity/finger1.svg' alt='finger1' className='w-[8.55px] h-[24.76px] top-[107.07px] left-[128.67px] absolute' />
                     <img src='clarity/finger2.svg' alt='finger2' className='w-[16.29px] h-[35.72px] top-[96.44px] left-[129.7px] absolute' />
                     <img src='clarity/finger3.svg' alt='finger3' className='w-[4px] h-[27.97px] top-[102.06px] left-[149.03px] absolute' />
-                    <img src='clarity/outerFace.svg' alt='outerFace' className='w-[98.25px] h-[131.74px] top-[66.3px] left-[33.06px] absolute' />
+                    <img src='clarity/hair.svg' alt='hair' className='w-[144.5px] h-[104.54px] top-[24.33px] left-[9.9px] absolute' />
+                    <img src='clarity/hairL.svg' alt='hairL' className='w-[145.53px] h-[105.41px] top-[24px] left-[9.44px] absolute' />
+                    <img src='clarity/color1.svg' alt='color1' className='w-[31.41px] h-[24.82px] top-[184.89px] left-[67.62px] absolute' />
+                    <img src='clarity/color1Line.svg' alt='color1Line' className='w-[32.38px] h-[25.87px] top-[184.26px] left-[67.17px] absolute' />
+                    <img src='clarity/sleeveLine.svg' alt='sleeveLine' className='w-[65.19px] h-[45.69px] top-[182.83px] left-[79.89px] absolute' />
+                    <img src='clarity/sleeve.svg' alt='sleeve' className='w-[64.26px] h-[44.77px] top-[183.33px] left-[80.31px] absolute' />
+                    <img src='clarity/buttonCover2.svg' alt='button2' className='w-[12.04px] h-[12.04px] top-[192.92px] left-[124.66px] absolute' />
+                    <img src='clarity/buttonCover.svg' alt='button1' className='w-[13.05px] h-[13.05px] top-[192.41px] left-[124.15px] absolute' />
+                    <img src='clarity/button.svg' alt='button' className='w-[13.05px] h-[13.05px] top-[190.55px] left-[124.49px] absolute' />
                     <img src='clarity/belowFace1.svg' alt='belowFace1' className='w-[5.42px] h-[19.12px] top-[108.15px] left-[126.95px] absolute' />
                     <img src='clarity/belowFace2.svg' alt='belowFace2' className='w-[2.16px] h-[10.34px] top-[98.71px] left-[130.33px] absolute' />
                     <img src='clarity/belowFace3.svg' alt='belowFace3' className='w-[68.34px] h-[100.93px] top-[98.46px] left-[63.69px] absolute' />
                     <img src='clarity/belowFace4.svg' alt='belowFace4' className='w-[23.46px] h-[4.86px] top-[82.09px] left-[56.37px] absolute' />
                     <img src='clarity/belowFace5.svg' alt='belowFace5' className='w-[27.02px] h-[3.04px] top-[72.09px] left-[86.29px] absolute' />
                     <img src='clarity/palm.svg' alt='palm' className='w-[83.22px] h-[106.31px] top-[92.29px] left-[76.25px] absolute' />
+                    <img src='clarity/ear.svg' alt='ear' className='w-[10.39px] h-[13.58px] top-[122.98px] left-[41.95px] absolute' />
+                    <img src='clarity/ear.svg' alt='ear' className='w-[10.39px] h-[13.58px] top-[122.98px] left-[41.95px] absolute' />
+                    <img src='clarity/colorShadow2.svg' alt='colorShadow2' className='w-[25.24px] h-[29.52px] top-[150.28px] left-[41.15px] absolute' />
+                    <img src='clarity/colorShadow2Line.svg' alt='colorShadow2Line' className='w-[26.27px] h-[30.5px] top-[149.76px] left-[40.62px] absolute' />
+                    <img src='clarity/colorShadow.svg' alt='colorShadow' className='w-[9.03px] h-[13.72px] top-[155.85px] left-[50.04px] absolute' />
+                    <img src='clarity/colorShadowLine.svg' alt='colorShadowLine' className='w-[10.02px] h-[14.72px] top-[155.36px] left-[49.58px] absolute' />
+                    <img src='clarity/outerFace.svg' alt='outerFace' className='w-[98.25px] h-[131.74px] top-[66.3px] left-[33.06px] absolute' />
                     <img src='clarity/face.svg' alt='face' className='w-[71.95px] h-[50.09px] top-[76.11px] left-[58.9px] absolute' />
-                    <img src='clarity/ear.svg' alt='ear' className='w-[10.39px] h-[13.58px] top-[122.98px] left-[41.95px] absolute' />
-                    <img src='clarity/ear.svg' alt='ear' className='w-[10.39px] h-[13.58px] top-[122.98px] left-[41.95px] absolute' />
+                    <img src='clarity/hairLine.svg' alt='hairLine' className='w-[32.98px] h-[47.11px] top-[36.57px] left-[6.93px] absolute' />
+                    <img src='clarity/hairLineLine.svg' alt='hairLineLine' className='w-[32.98px] h-[47.11px] top-[36.57px] left-[6.93px] absolute' />
+                    <img src='clarity/hairLine2.svg' alt='hairLine2' className='w-[46.74px] h-[10.65px] top-[22.47px] left-[77.69px] absolute' />
+                    <img src='clarity/hairLine2Line.svg' alt='hairLine2Line' className='w-[47.6px] h-[11.68px] top-[22px] left-[77.18px] absolute' />
+                    <img src='clarity/hairLine3.svg' alt='hairLine3' className='w-[35.49px] h-[24.94px] top-[23.75px] left-[40.68px] absolute' />
+                    <img src='clarity/hairLine3Line.svg' alt='hairLine3Line' className='w-[36.53px] h-[25.99px] top-[23.27px] left-[40.17px] absolute' />
+                    <img src='clarity/color2.svg' alt='color2' className='w-[14.92px] h-[31px] top-[194.99px] left-[54.39px] absolute' />
+                    <img src='clarity/color2Line.svg' alt='color2Line' className='w-[15.88px] h-[31.89px] top-[194.55px] left-[53.89px] absolute' />
+                    <img src='clarity/color3.svg' alt='color3' className='w-[12.71px] h-[10.52px] top-[195px] left-[64.19px] absolute' />
+                    <img src='clarity/color3Line.svg' alt='color3Line' className='w-[13.64px] h-[11.47px] top-[194.55px] left-[63.69px] absolute' />
                 </div>
             </div>)
             }
